0d5e2a344385cbe189fbda97449a03623fd5075a
lilcappucc <smt>
2019-07-31 16:24:54 +0300

    ColorPicker / TableView
    add table view
    comment color picker
    change background
    load from files

---
+++ MyNotes/Support/UIViewControllerExtension.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewControllerExtension.swift
+//  MyNotes
+//
+//  Created by dewill on 31/07/2019.
+//  Copyright © 2019 dewill. All rights reserved.
+//
+
+import UIKit
+
+class UIViewControllerExtension: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
